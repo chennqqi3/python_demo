b0642ade219895f3620c3cff2e053d3de619b5b4
weidongdong <weidongdong@360.cn>
2017-07-10 18:16:57 +0800

    自动评论功能完成

--- src/test/tool/csdn/auto_evaluate.py
+++ src/test/tool/csdn/auto_evaluate.py
@@ -1,9 +1,12 @@
 # 自动填写csdn评价
 import re
 # 登录地址
+import threading
 import time
 
 import requests
+
+from src.test.tool.csdn.download_item import DownloadItem
 
 login_url = 'https://passport.csdn.net/account/login?from=http%3A%2F%2Fmy.csdn.net%2Fmy%2Fmycsdn'
 
@@ -17,6 +20,9 @@ headers = {
 
 # 使用session保持会话状态
 session = requests.session()
+
+success_cnt = 0
+lock = threading.Lock()
 
 
 # 打开登录页面获取页面上的lt和execution两个参数
@@ -81,41 +87,85 @@ def open_download_page():
         print('下载页面打开成功')
     # print(download_list_res.text)
 
-    download_url_list = []
+    download_item_list = []
 
     match_page = re.search(re.compile('<div class="page_nav">共.*?个.*&nbsp; 共(.*?)页', re.S), download_list_res.text)
     page_cnt = match_page.group(1)
     print('共有' + page_cnt + '页')
 
     # 循环每一个，获取待评价的项的访问链接到数组
-    page_loop(page_cnt, download_url_list)
-    # print(download_url_list)
+    page_loop(page_cnt, download_item_list)
+    # print(download_item_list)
 
+    print('共有' + str(len(download_item_list)) + '项待评论资源')
+
+    ls_thread = []
     # 循环数组，访问待评价页面
-    for download_uri in download_url_list:
-        download_page_res = session.get(domain + download_uri, headers=headers)
+    for download_item in download_item_list:
+        # open_item_url(download_item)
+        # submit_evaluate(download_item.id)
+        # 每次评论需要间隔60s
+        # time.sleep(60)
+
+        thr = threading.Thread(target=submit_evaluate, args=(download_item.id,))
+        thr.start()
+        ls_thread.append(thr)
+
+    # 等待执行完毕
+    for th in ls_thread:
+        th.join()
+
+    print('脚本执行完成！成功评论'+str(success_cnt)+'条资源')
 
 
+# 打开下载项页面
+# def open_item_url(download_item):
+#     print('打开下载项页面')
+#     download_page_res = session.get(domain + download_item.uri, headers=headers)
+#
+#     if 200 == download_page_res.status_code:
+#         print(str(download_item.id) + '详情页打开成功')
+
+# 提交评价
+# 对于跨域请求，Python不需要模拟jsonp，直接同普通方式请求即可
 def submit_evaluate(id):
     # 提交参数
-    jsonpcallback = ''
+    # jsonpcallback = ''
     sourceid = id
     # 评论内容
     content = '很好的学习资料，谢谢分享'
     txt_validcode = 'undefined'
     # 打分
-    rating = 5
+    rating = str(5)
     # 应该是提交的时间戳
     t = str(int(round(time.time() * 1000)))
     # 应该是打开页面的时间戳
     _ = str(int(round(time.time() * 1000)))
 
     # 提交URL
-    evaluate_url = 'http://download.csdn.net/index.php/comment/post_comment?jsonpcallback=jQuery111104839852002988987_1499424460935&sourceid=9748056&content=%E8%B0%A2%E8%B0%A2%E5%88%86%E4%BA%AB%EF%BC%8C%E5%BE%88%E5%A5%BD%E7%9A%84%E5%AD%A6%E4%B9%A0%E8%B5%84%E6%96%99&txt_validcode=undefined&rating=5&t=1499424636324&_=1499424460940'
+    evaluate_url = 'http://download.csdn.net/index.php/comment/post_comment' \
+                   '?sourceid=' + sourceid + \
+                   '&content=' + content + \
+                   '&txt_validcode=' + txt_validcode + \
+                   '&rating=' + rating + \
+                   '&t=' + t + \
+                   '&_=' + _
+    evaluate_res = session.get(evaluate_url, headers=headers)
+
+    global success_cnt
+    if 200 == evaluate_res.status_code:
+        match_evlu = re.search(re.compile('{"succ":1}'), evaluate_res.text)
+        if match_evlu:
+            print(id + '评价成功！')
+            lock.acquire()
+            try:
+                success_cnt += 1
+            finally:
+                lock.release()
 
 
 # 打开每页列表
-def page_loop(page_cnt, download_url_list):
+def page_loop(page_cnt, download_item_list):
     for page in range(0, int(page_cnt)):
         page_s = str(page + 1)
         print('打开第' + page_s + '页')
@@ -133,20 +183,22 @@ def page_loop(page_cnt, download_url_list):
             # print(lis)
 
             # 获取个待评价项的链接
-            get_item_url_by_li(download_url_list, lis)
+            get_item_url_by_li(download_item_list, lis)
 
 
 # 循环每个下载项，并将子项的打开链接保存在数组中
-def get_item_url_by_li(download_url_list, lis):
+def get_item_url_by_li(download_item_list, lis):
     print('解析下载项的打开链接')
     for li in lis:
         # 匹配每一项里的"立即评价"的链接
         match_li = re.search(
-            re.compile('<div class="flag">.*<a target="_blank" href="(/detail/.*?#comment)">立即评价</a>', re.S), li)
+            re.compile(
+                '<h3>.*href="/detail/.*/(.*?)".*</h3>.*<div class="flag">.*<a target="_blank" href="(/detail/.*?#comment)">立即评价</a>',
+                re.S), li)
         if match_li:
             # print(match_li.group(1))
             # 保存评价地址到数组
-            download_url_list.append(match_li.group(1))
+            download_item_list.append(DownloadItem(match_li.group(1), match_li.group(2)))
 
 
 if __name__ == '__main__':
